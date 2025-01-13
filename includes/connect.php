f3e2111610e75e66c477d7ea722b9b1caf7aea88
rahmanS1d1q <rahmanshiddiq09@gmail.com>
2025-01-13 23:09:22 +0700

    add

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("153.92.15.44", "u523791406_wethrift", "", "u523791406_wethrift");
+$con = mysqli_connect("srv1785.hstgr.io", "u523791406_wethrift", "Second123.", "u523791406_wethrift");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
